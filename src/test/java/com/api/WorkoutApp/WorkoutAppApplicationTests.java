6491321bfe3afc13b2f27a337ebed11d0efd8cb8
eusouagabrielle <rodriguesgabrielle007@gmail.com>
2022-12-10 14:02:17 +0100

    Initial commit

---
+++ src/test/java/com/api/WorkoutApp/WorkoutAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.WorkoutApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
